b26dfc3ae4b0e40c9515f460659099d8dfe95b13
ftarantelli <francesco.tarantelli@phd.unipi.it>
2025-07-03 03:18:08 +0200

    Start: first page form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import icon     from 'astro-icon';
 
 export default defineConfig({
   output: 'static',
   build: { outDir: './docs' },
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   site: 'https://normaxlabs.github.io',   // URL pubblico (importante per i link absolute)
 });
