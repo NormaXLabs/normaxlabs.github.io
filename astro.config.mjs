47ceb438a7ae2ae3f04bfdef47948cd9d77632c5
ftarantelli <francesco.tarantelli@phd.unipi.it>
2025-07-03 02:59:22 +0200

    fix: astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ export default defineConfig({
   output: 'static',
   build: { outDir: './docs' },
   integrations: [tailwind()],
-  site: 'https://<org>.github.io',   // URL pubblico (importante per i link absolute)
+  site: 'https://normaxlabs.github.io',   // URL pubblico (importante per i link absolute)
 });
