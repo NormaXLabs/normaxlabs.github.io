92f41874c0daf584ec86f43541da3273a3559fc6
ftarantelli <francesco.tarantelli@phd.unipi.it>
2025-07-07 01:45:25 +0200

    Add main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import icon     from 'astro-icon';
+import Icons     from 'astro-icon';
 
 export default defineConfig({
   output: 'static',
   build: { outDir: './docs' },
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), Icons({
+      collections: {
+        //   nome set : percorso cartella
+        local: './src/icons',
+      },
+    })],
   site: 'https://normaxlabs.github.io',   // URL pubblico (importante per i link absolute)
 });
